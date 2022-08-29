a6d98598ea6fd093f688621efd326901e30e76f9
swittuthcoding <swittuthcoding@gmail.com>
2022-08-29 17:11:06 -0400

    update basics for kanban board

--- src/board-components/Column.js
+++ src/board-components/Column.js
@@ -1,5 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 8x;
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+`;
+const Title = styled.h3`
+  padding: 8px;
+`;
+const TaskList = styled.div`
+  padding: 8px;
+`;
 
 export function Column(props) {
-  return <>{props.column.title}</>;
+  return (
+    <Container>
+      <Title>{props.column.title}</Title>
+      <TaskList>Tasks go here</TaskList>
+    </Container>
+  );
 }
