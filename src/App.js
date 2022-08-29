b222eac8a8c25733410971f71b5c1d0e206aecf0
swittuthcoding <swittuthcoding@gmail.com>
2022-08-29 16:24:35 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-
 import React from "react";
-import Button from '@material-ui/core/Button';
+import { Button } from "@mui/material";
 
 class App extends React.Component {
   render() {
     const { name } = this.props;
     return (
       <>
-        <h1>
-          Hello {name}
-        </h1>
+        <h1>Hello {name}</h1>
         <Button variant="contained">this is a material UI button</Button>
       </>
     );
